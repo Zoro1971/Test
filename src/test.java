48ed26a3bb5eae940715b234611212d659a9f589
Zoro Zoro <zoro1971@list.ru>
2017-12-24 19:42:53 +0700

    Initial commit

---
+++ src/test.java
@@ -0,0 +1,67 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public  class test {
+
+
+
+
+
+
+
+    }
+
+        public static void main(String[] args)  {
+            System.out.println("dfcz");
+
+
+
+
+//        B<Number> o = new B<Integer>();
+
+//        A<Integer> 04 = new B<Integer>();
+
+
+        }
+
+
+}
+
+//public class test extends A {
+//
+//
+//        private int j = 3;
+//
+//        @Override
+//        public void setI() {
+//            i = j;
+//        }
+//
+//
+//        public static void main(String[] args) {
+//
+//            System.out.println("test");
+//
+//            test b = new test();
+//            System.out.println(b.getI());
+//
+//        }
+//    }
+//
+//class A {
+//        protected int i = 1;
+//
+//        public int getI() {
+//            return i;
+//        }
+//
+//        public A() {
+//            setI();
+//        }
+//
+//        public void setI() {
+//            i = 2;
+//        }
+//    }
+//
