13408c82ba88d88ada620fcc3ccf6774af4cdded
Zoro1971 <Zoro1971@list.ru>
2017-12-25 23:13:43 +0700

    Вариант 2-2

--- src/test.java
+++ src/test.java
@@ -15,6 +15,7 @@ public  class test {
         public static void main(String[] args)  {
             System.out.println("первый вариант");
             System.out.println("второй вариант");
+            System.out.println("второй вариант - 2");
 
 
 
